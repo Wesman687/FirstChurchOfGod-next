cb5ed4a56070fb057c6e4a1128fdc9eda4372f37
Wesman687 <wesman687@gmail.com>
2024-09-27 03:06:24 -0400

    added delete function for gallery images

---
+++ components/gallery/AddImages.jsx
@@ -0,0 +1,101 @@
+import React, { useRef, useState } from 'react'
+import UploadIcon from '../icons/UploadIcon'
+import { db, storage } from '@/firebase'
+import { getDownloadURL, ref, uploadString } from 'firebase/storage'
+import { addDoc, collection } from 'firebase/firestore'
+
+function AddImages({ filters }) {
+    const [image, setImage] = useState()
+    const filePickerRef = useRef(null)
+    const [filter, setFilter] = useState(filters[0])
+    const [desc, setDesc] = useState('')
+    function addImage(e) {
+        const reader = new FileReader()
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+
+        reader.addEventListener("load", e => {
+            setImage(e.target.result)
+        })
+        
+    }
+    
+    async function handleAdd(){
+        let downloadURL
+            if (image) {
+                const string = generateRandomStringWithDate()
+                console.log(string)
+                const imageRef = await ref(storage, `images/${string}`)
+                const uploadImage = await uploadString(imageRef, image, "data_url")
+                downloadURL = await getDownloadURL(imageRef)
+         }
+         const docRef = await addDoc(collection(db, 'images'), {
+            link: downloadURL,
+            timeStamp: new Date(),
+            gallery: filter,
+            desc,
+        })
+        setImage(null)
+        setDesc('')
+
+
+    }
+    function generateRandomStringWithDate() {
+        const now = new Date();
+        const timestamp = now.getTime(); // Get milliseconds since epoch
+        const randomPart = Math.random().toString(36).substr(2, 6); // Generate random string
+      
+        return `image_${timestamp}_${randomPart}`;
+      }
+    
+    return (
+        <div className='add-image-container'>
+            <div>
+                {image ?
+                    <div className='add-image-wrapper' onClick={() => filePickerRef.current.click()}>
+                        <img src={image} alt={'new image'} className='new-image' />
+                        <input onChange={addImage} ref={filePickerRef} className="hidden" type="file" />
+
+                    </div>
+                    :
+                    <div className='add-image-template-wrapper'>
+                        <div className='add-image-template' onClick={() => filePickerRef.current.click()}>
+                            <label>2400 x 1200</label>
+                            <div className='add-image-upload-wrapper' >
+                                <UploadIcon />
+                                
+                            </div>
+                        </div>
+                        <input onChange={addImage} ref={filePickerRef} className="hidden" type="file" />
+                    </div>}
+            </div>
+            {image &&
+                <div className='add-image-details-container'>
+                    <div className='add-image-details-wrapper'>
+                        <div>
+                    <label className='add-image-labels'>Select Gallery: </label>
+                    <select onChange={(e) => setFilter(e.target.value)} value={filter} className='gallery-filter-box'>                        
+                        {filters.length > 0 && filters.map((item, index) => (
+                            <>
+                                <option value={item} key={index}>{item}</option>
+                            </>))}
+                    </select>
+                    </div>
+                    <div>
+                        <button className='add-image-button' onClick={handleAdd}>Add Image</button>
+                    </div>
+                    </div>
+                    <div>
+                        <label className='add-image-labels'>Desc:</label>
+                        <textarea className='add-image-desc' value={desc} onChange={(e)=>setDesc(e.target.value)} placeholder='Description' />
+                    </div>
+
+                </div>}
+
+
+        </div>
+    )
+}
+
+export default AddImages
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,63 +1,136 @@
+import AddImages from '@/components/gallery/AddImages'
 import ManageFilters from '@/components/gallery/ManageFilters'
 import ArrowUturnLeft from '@/components/icons/ArrowUturnLeftIcon'
 import Layout from '@/components/Layout'
 import AccountInfo from '@/components/modals/AccountInfo'
 import Login from '@/components/modals/Login'
-import { auth } from '@/firebase'
+import { auth, db, storage } from '@/firebase'
 import { signOutUser } from '@/redux/userSlice'
 import { signOut } from 'firebase/auth'
+import { collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query, where } from 'firebase/firestore'
+import { deleteObject, ref } from 'firebase/storage'
 import Link from 'next/link'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-const imageArray = [{
-    image: 'http://placehold.it/2400x1600',
-    category: 'EVENTS'
-}, {
-    image: 'https://newlife-church-html.axiomthemes.com/images/gallery-post-4.jpg',
-    category: 'EVENTS'
-}, {
-    image: 'https://newlife-church-html.axiomthemes.com/images/participate-with-us.jpg',
-    category: 'WORSHIP'
-}, {
-    image: 'https://newlife-church-html.axiomthemes.com/images/our-community.jpg',
-    category: 'WORSHIP'
-}
-]
-const filters = ['ALL', 'EVENTS', 'WORSHIP']
+
+
 
 function Gallery() {
     const [filter, setFilter] = useState('ALL')
-    const [displayedImages, setDisplayedImages] = useState(imageArray || [])
+    const [displayedImages, setDisplayedImages] = useState([])
     const user = useSelector((state) => state.user)
     const dispatch = useDispatch()
+    const [images, setImages] = useState([])
     const [displayGallery, setDisplayGallery] = useState(true)
-    const [displayFilters, setDisplayFilters] = useState('')
+    const [displayFilters, setDisplayFilters] = useState(false)
+    const [displayAddImages, setDisplayAddImages] = useState()
+    const [filters, setFilters] = useState([])
     async function logOut(){
         await signOut(auth)
         dispatch(signOutUser())
     }
+    function handleEdit(){
+        
+
+    }
+    async function handleDelete(item){
+        const temp = item.link.slice(93)
+        const imageName = temp.slice(0, temp.indexOf('?'))
+        const imageRef = await ref(storage, `images/${imageName}`)
+        await deleteObject(imageRef)
+        let imagesRef = await collection(db, "images");
+        const q = await query(imagesRef, where("link", "==", item.link)); 
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+            querySnapshot.forEach(async (docSnapshot) => {
+                const docRef = doc(db, 'images', docSnapshot.id);                
+                await deleteDoc(docRef);
+            });             
+            const q2 = await query(collection(db, 'images'))
+            const unsubscribe = onSnapshot(q2, (snapshot) =>{
+                const data = snapshot.docs
+                const inputs = mapLinks(data)
+                setImages(data.map((item)=>item.data()))
+                setDisplayedImages(inputs)
+            })
+        } else {
+            console.log('No document found with that link.');
+        }
+    
+}
+
+
+    
     function handleFilter(e) {
         const value = e.target.value
         setFilter(value)
         if (value === 'ALL') {
-            setDisplayedImages(imageArray)
+            setDisplayedImages(images)
+            
         }
         else {
-            setDisplayedImages(imageArray.filter((item) => item.category === value))
+            setDisplayedImages(images.filter((item) => {
+                if (item.gallery === value) {
+                    return item.link
+                }
+            }))
         }
     }
     function handleManageFilters(){
+        if (displayAddImages) {
+            setDisplayAddImages(false)
+        }
         if (user.isAdmin){
             setDisplayGallery(false)
             setDisplayFilters(true)
         }
         
     }
+    function handleAddImage(){
+        if (displayFilters){
+            setDisplayFilters(false)
+        }
+        if (user.isAdmin){
+            setDisplayGallery(false)
+            setDisplayAddImages(true)
+        }
+    }
     function goBack(){
         setDisplayGallery(true)
-        setDisplayFilters(false)
+        if (displayAddImages){
+            setDisplayAddImages(false)
+        }
+        else if (displayFilters){
+            setDisplayFilters(false)
+        }
+        
     }
+    function mapLinks(data){
+        return data.map((item)=>( item.data()))
+    }
+
+    useEffect(()=>{
+        const q = query(collection(db, 'filters'))                
+        const unsubscribe = onSnapshot(q, (snapshot) =>{
+            const data = snapshot.docs
+            const inputs = data.map((item)=>( item.data().filter))
+            setFilters(inputs)
+        })
+        return unsubscribe
+    },[])
+    useEffect(()=> {
+        const q2 = query(collection(db, 'images'))          
+        const unsubscribe = onSnapshot(q2, (snapshot) =>{
+            const data = snapshot.docs
+            const inputs = mapLinks(data)
+            setImages(data.map((item)=>item.data()))
+            setDisplayedImages(inputs)
+        })
+        return unsubscribe
+    }, [])
+    console.log(displayedImages)
 
     return (
         <>
@@ -87,12 +160,24 @@ function Gallery() {
                                                     <div className='manage-filter-container'>
                                                     <select onChange={(e) => handleFilter(e)} value={filter} className='gallery-filter-box'>
                                                         <option value='ALL'>All</option>
-                                                        <option value='EVENTS'>EVENTS</option>
-                                                        <option value='WORSHIP'>WORSHIP</option>
+                                                        {filters.length > 0 && filters.map((item, index) => (
+                                                            <>
+                                                        <option value={item} key={index}>{item}</option>
+                                                        </>))}
                                                     </select>
-                                                    <div className='manage-filter-arrow-container'>
-                                                        <div>
-                                                    {user.isAdmin && <label className='manage-filters-label' onClick={handleManageFilters}>Manage Filters</label>}
+                                                    {user.isAdmin && <div className='manage-filter-arrow-container'>
+                                                        <div className='manage-buttons-container'>
+                                                            <div>                                                    
+                                                    {!displayFilters ? <label className='manage-filters-label' onClick={handleManageFilters}>Manage Gallery Names</label> :
+                                                    <label className='manage-filters-label-active'>Manage Gallery Names</label>                                                 
+                                                    }
+                                                    </div>
+                                                    <div>
+                                                    
+                                                    {!displayAddImages ? <label className='manage-filters-label' onClick={handleAddImage}>Add Images</label> :
+                                                    <label className='manage-filters-label-active'>Add Images</label>                                                 
+                                                    }
+                                                    </div>
                                                     </div>
                                                     
                                                     {displayFilters && 
@@ -101,7 +186,13 @@ function Gallery() {
                                                         <ArrowUturnLeft />
                                                     </div>
                                                     </>}
+                                                    {displayAddImages && 
+                                                    <>                                                    
+                                                    <div className='gallery-back-arrow' onClick={goBack}>
+                                                        <ArrowUturnLeft />
                                                     </div>
+                                                    </>}
+                                                    </div>}
                                                     
                                                     </div>
                                                     
@@ -124,14 +215,19 @@ function Gallery() {
                                                 </div>
                                             </article>
                                             <div className='gallery-image-container'>
-                                                {(imageArray.length > 0 && displayGallery) && displayedImages.map((item, index) => (
+                                                {(displayedImages.length > 0 && displayGallery) && displayedImages.map((item, index) => (
                                                     <div className="esg-media-cover-wrapper" key={index}>
                                                         <div className="esg-entry-media">
-                                                            <img src={item.image} alt="" />
+                                                            <img src={item.link} alt="" />
+                                                        </div>
+                                                        <div>
+                                                            <label className='gallery-image-label' onClick={()=>handleEdit(item)}>Edit</label>
+                                                            <label className='gallery-image-label' onClick={()=>handleDelete(item)}>Delete</label>
                                                         </div>
                                                     </div>
                                                 ))}
                                                 {displayFilters && <ManageFilters />}
+                                                {displayAddImages && <AddImages filters={filters} />}
                                             </div>
                                         </div>
                                     </article>
