692845961e3457d9a33ddf82ebe06f4abbb913c9
Wesman687 <wesman687@gmail.com>
2024-09-27 22:33:57 -0400

    cwc page made

--- pages/cwc/index.js
+++ pages/cwc/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import bibleStudy from '@/images/cwc/bible-study.jpg'
 import Image from 'next/image'
 import bgimage from '@/images/cwc/christian-womens-connection.jpg'
+import Link from 'next/link'
 
 function CWC() {
     return (
@@ -18,7 +19,7 @@ function CWC() {
                     <div className='cwc-intro-wrapper'>
                         <h3>Discover a global network of women ready to live out their calling.</h3>
                         <h5>As a Christian Women Connection Member, you are grafted into the blessing of leadership training, spiritual growth, and fellowship.</h5>
-                        <button className='cwc-member-button light-blue-button'>Become a member</button>
+                        <Link href={'https://christianwomenconnection.org/connection/'} target='_blank'><button className='cwc-member-button light-blue-button'>Become a member</button></Link>
                     </div>                   
                 </div>
                 <h5 className='cwc-body-text'>You do not have to be a member of the Church of God to join or participate in.
@@ -26,13 +27,16 @@ function CWC() {
                         If you are interested in joining and/or visiting our group and have any question, please call Susan at <a href='tel:#3865460143'>386-546-0143</a> 
                 </h5>
                 <div className='cwc-mission-container'>
-                    <div className='cwc-mission-wrapper'>
+                    <div className='cwc-mission-wrapper' >
                     <h3>Our Mission</h3>
-                    <h4>When you improve the lives of women, you change the world.</h4>
-                    <h4>We exist to build relationships with women everywhere. </h4>
-                    <h4>We provide ministry resources to the Church of God and to other groups to connect women through relationships, spiritual formation, and service.</h4>
-                    <h4>We support others as they train and empower women for leadership in peace building and reconciliation.</h4>
+                    <ul>
+                    <li>When you improve the lives of women, you change the world.</li>
+                    <li>We exist to build relationships with women everywhere. </li>
+                    <li>We provide ministry resources to the Church of God and to other groups to connect women through relationships, spiritual formation, and service.</li>
+                    <li>We support others as they train and empower women for leadership in peace building and reconciliation.</li>
+                    </ul>
                     </div>
+                    <Link href={'https://christianwomenconnection.org/connection/'} target='_blank'><button className='cwc-visit-button orange-btn click'>Visit Us</button></Link>
                 </div>
             </div>
         </Layout>
