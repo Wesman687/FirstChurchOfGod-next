165c4589069cc5aa2e573e3d014e49f583c63d50
Wesman687 <wesman687@gmail.com>
2024-10-11 00:16:44 -0400

    added like components

--- components/feed/Post.jsx
+++ components/feed/Post.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { deleteDoc, doc, getDoc } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
 import { db, storage } from '@/firebase'; // Import Firestore instance
 import ChevronDownIcon from '../icons/ChevronDownIcon';
 import ChevronUpIcon from '../icons/ChevronUpIcon';
@@ -12,33 +12,40 @@ import ChatIcon from '../icons/ChatIcon';
 import ShareIcon from '../icons/ShareIcon';
 import { deleteObject, ref } from 'firebase/storage';
 import { toast } from 'react-toastify';
+import SolidThumbsUp from '../icons/SolidThumbsUp';
 
 function Post({ post }) {
     const [owner, setOwner] = useState(null);
     const [edit, setEdit] = useState(false)
     const [collapsePost, setCollapsePost] = useState(false)
     const [showConfirm, setShowConfirm] = useState(false); // State for showing the confirmation modal
+    const [likes, setLikes] = useState(post.likes || []);
+    const [comments, setComments] = useState(post.comment || []);
     const user = useSelector(state => state.user)
     useEffect(() => {
         // Fetch the user from the Firestore users collection
         const fetchUser = async () => {
-            if (post.owner) {
+            if (post.owner) {                
                 try {
                     const userDocRef = doc(db, 'user', post.owner); // Assuming the user ID matches the document ID
                     const userSnapshot = await getDoc(userDocRef);
                     if (userSnapshot.exists()) {
                         setOwner(userSnapshot.data());
                     } else {
-                        console.log('User not found');
+                        toast.error('User not found');
                     }
                 } catch (error) {
-                    console.error('Error fetching user:', error);
+                    toast.error('Error fetching user:', error);
                 }
             }
         };
 
         fetchUser();
     }, [post.owner]);
+    useEffect(()=>{
+        setLikes(post.likes)
+        setComments(post.comments)
+    },[post.likes, post.comments])
     const confirmRemoveMember = () => {
         setShowConfirm(true);
     };
@@ -66,10 +73,22 @@ function Post({ post }) {
             toast.error('There was an error with deleting the post')
         }
     }
-    function handleLikes() {
-        console.log('test')
-        return '0'
-    }
+    async function handleLikes(e) {
+        e.stopPropagation();
+        if (!user.username) {
+          return;
+        }
+        if (likes?.includes(user.uid)) {
+          await updateDoc(doc(db, "posts", post.id), {
+            likes: arrayRemove(user.uid),
+          });
+        } else {
+          await updateDoc(doc(db, "posts", post.id), {
+            likes: arrayUnion(user.uid),
+          });
+        }
+      }
+      console.log(post)
     return (
         <>
             {collapsePost ? <div className='post-container-collapsed click' onClick={() => setCollapsePost((prev) => !prev)}><ChevronDownIcon classes={'white icon-small'} /><div className='collapse-line'></div></div> :
@@ -115,11 +134,11 @@ function Post({ post }) {
                     <div className='post-footer'>
                         <div className='post-likes'>
 
-                            <label>{handleLikes}0</label>
+                            <label>{likes?.length > 0 && likes.length}</label>
                         </div>
                         <div className='post-like-container'>
-                            <div className='click'>
-                                <ThumbsUpIcon classes={'xs-icon'} />
+                            <div className='click' onClick={handleLikes}>
+                                {likes?.includes(user.uid) ? <SolidThumbsUp classes={'xs-icon white'} /> : <ThumbsUpIcon classes={'xs-icon'} />}
                                 <label className='click'> Like</label>
                             </div>
                             <div className='click'>
