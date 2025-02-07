f6dd9135a7e5e50c8839113d99ed2369816039d8
Wesman687 <wesman687@gmail.com>
2025-02-06 20:30:47 -0500

    update

--- components/home/HomeToAction.jsx
+++ components/home/HomeToAction.jsx
@@ -5,27 +5,19 @@ import Link from 'next/link'
 
 function HomeToAction() {
     return (
-        <div className="hp1_call_to_action_section">
-            <div className="sc_call_to_action sc_call_to_action_style_1 sc_call_to_action_align_left sc_call_to_action_color_style_1 width_100_per">
-                <div className="content_wrap">
-                    <div className="columns_wrap">
-                        <div className="cwc-homepage-image-wrapper">
-                            <Image src={cwc} className='cwc-homepage-image' alt=""  />
-                        </div>
-                        <div className="home-cwc-wrapper">
-                            
-                            <h2 className="home-cwc-title">Christian Women Connection</h2>
-                            <div className="home-cwc-paragraph">Discover a global network of women ready to live out their calling.</div>
-                            <div className="sc_call_to_action_buttons sc_item_buttons">
-                                <div className="sc_call_to_action_button sc_item_button home-cwc-button-wrapper">
-                                    <Link href={'/cwc'}>
-                                    <label className="click sc_button sc_button_square sc_button_style_filled sc_button_color_style_2 sc_button_size_large orange-btn home-cwc-button">view more</label>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <div className="home-action-container">
+            <div className="columns_wrap">
+                <div className="cwc-homepage-image-wrapper">
+                <Link className='home-cwc-buttons-wrapper' href={'/cwc'}>
+                    <Image src={cwc} className='cwc-homepage-image' alt="" />
+                    </Link>
+                    
                 </div>
+                <div className="home-cwc-wrapper">
+                    <h2 className="home-cwc-title">Christian Women Connection</h2>
+                    <div className="home-cwc-paragraph">Discover a global network of women ready to live out their calling.</div>
+                </div>
+
             </div>
         </div>
     )
