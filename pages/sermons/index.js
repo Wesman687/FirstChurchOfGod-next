95c3a1804aa665fd36ddd24bb8822511c68a3c25
Wesman687 <wesman687@gmail.com>
2025-02-27 11:54:30 -0500

    stupid apostrophes

--- pages/sermons/index.js
+++ pages/sermons/index.js
@@ -13,7 +13,7 @@ function About() {
                         <source src="https://egxh7vl5cuacnn7o.public.blob.vercel-storage.com/975f47e4-9206-4251-8de2-c87271f75906/deb2-9-25-twjHOOzCGc8HKrByfcm4ikiSuuKibD.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
-                    <h5>{`The video features a heartfelt speech by Deb Gornoski, who shares her journey of stepping out of her comfort zone to engage in a strip club ministry. She emphasizes the importance of leaving one&apos;s comfort zone to reach out to those who are often marginalized or overlooked, drawing inspiration from Jesus example of associating with sinners...`}</h5>
+                    <h5>{`The video features a heartfelt speech by Deb Gornoski, who shares her journey of stepping out of her comfort zone to engage in a strip club ministry. She emphasizes the importance of leaving ones comfort zone to reach out to those who are often marginalized or overlooked, drawing inspiration from Jesus example of associating with sinners...`}</h5>
                     </div>
 
             </div>
