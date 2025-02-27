94ee43a4d763462cd7692e9a6ce0913be109f15a
Wesman687 <wesman687@gmail.com>
2025-02-27 18:43:26 -0500

    changed video url

--- pages/sermons/index.js
+++ pages/sermons/index.js
@@ -10,7 +10,7 @@ function About() {
                 <div className="video-container">
                     <h2>Stepping out your comfort zone</h2>
                     <video width="100%" controls>
-                        <source src="https://egxh7vl5cuacnn7o.public.blob.vercel-storage.com/975f47e4-9206-4251-8de2-c87271f75906/deb2-9-25-twjHOOzCGc8HKrByfcm4ikiSuuKibD.mp4" type="video/mp4" />
+                        <source src="https://paulwesleymiracle.nyc3.cdn.digitaloceanspaces.com/paulwesleymiracle/firstchurchofgod/deb2-9-25.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
                     <h5>{`The video features a heartfelt speech by Deb Gornoski, who shares her journey of stepping out of her comfort zone to engage in a strip club ministry. She emphasizes the importance of leaving ones comfort zone to reach out to those who are often marginalized or overlooked, drawing inspiration from Jesus example of associating with sinners...`}</h5>
