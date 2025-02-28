38fb2d2c83e3e9dbe7e1f52bdc2df0ab0a2d5192
Wesman687 <wesman687@gmail.com>
2025-02-27 20:01:32 -0500

    fixed a type

--- pages/sermons/index.js
+++ pages/sermons/index.js
@@ -8,7 +8,7 @@ function About() {
             <div className="sermon-container">
                 <h1>Sermons</h1>
                 <div className="video-container">
-                    <h2>Stepping out your comfort zone</h2>
+                    <h2>Stepping out of your comfort zone</h2>
                     <video width="100%" controls>
                         <source src="https://paulwesleymiracle.nyc3.cdn.digitaloceanspaces.com/paulwesleymiracle/firstchurchofgod/deb2-9-25.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
